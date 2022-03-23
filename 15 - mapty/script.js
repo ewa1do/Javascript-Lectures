98fb1a0b67071d44a53dfdef0f946689c580ae8c
ewa1do <this.eduardovera@gmail.com>
2022-03-23 11:02:28 -0400

    Displaying a map maker

--- 15 - mapty/script.js
+++ 15 - mapty/script.js
@@ -31,14 +31,28 @@ if (navigator.geolocation) {
             position.coords.longitude
         ], 13);
 
+        // console.log(map);
+
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+        }).addTo(map)
 
-        L.marker(coords).addTo(map)
-            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        map.on('click', function (mapEvent) {
+            console.log(mapEvent);
+
+            const { lat, lng } = mapEvent.latlng
+
+            L.marker([lat, lng]).addTo(map)
+            .bindPopup(L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup',
+            }))
+            .setPopupContent('Workout')
             .openPopup();
-
+        })
 
     }, function () {
         alert(`Could not get your position`);
