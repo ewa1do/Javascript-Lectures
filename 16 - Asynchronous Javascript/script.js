f107de0b00f979791847dffc5e930d45f3e8f5e7
ewa1do <this.eduardovera@gmail.com>
2022-03-27 12:38:41 -0400

    consuming promises

--- 16 - Asynchronous Javascript/script.js
+++ 16 - Asynchronous Javascript/script.js
@@ -63,54 +63,86 @@ const renderCountry = function (data, className = '') {
     countriesContainer.style.opacity = '1';
 }
 
-const getCountryAndNeighbour = function (country) {
+// const getCountryAndNeighbour = function (country) {
     
-    // AJAX call country 1
-    const request = new XMLHttpRequest();
-    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-    request.send();
+//     // AJAX call country 1
+//     const request = new XMLHttpRequest();
+//     request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//     request.send();
 
-    request.addEventListener('load', function () {
-        const [data] = JSON.parse(this.responseText);
-        console.log(data);
-        console.log(Object.entries(data.currencies)[0][1].name);
+//     request.addEventListener('load', function () {
+//         const [data] = JSON.parse(this.responseText);
+//         console.log(data);
+//         console.log(Object.entries(data.currencies)[0][1].name);
         
-        // Render country 1 
-        renderCountry(data);
+//         // Render country 1 
+//         renderCountry(data);
 
-        // Get neighbour country (2)
-        const [neighbour] = data.borders;
+//         // Get neighbour country (2)
+//         const [neighbour] = data.borders;
 
 
-        if (!neighbour) return;
+//         if (!neighbour) return;
 
-        // AJAX call country 2
-        const request2 = new XMLHttpRequest();
-        request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
-        request2.send();
+//         // AJAX call country 2
+//         const request2 = new XMLHttpRequest();
+//         request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+//         request2.send();
 
-        request2.addEventListener('load', function () {
-            const [data2] = JSON.parse(this.responseText);
+//         request2.addEventListener('load', function () {
+//             const [data2] = JSON.parse(this.responseText);
 
-            // console.log(data2);
-            renderCountry(data2, 'neighbour');
-        })
-    });
+//             // console.log(data2);
+//             renderCountry(data2, 'neighbour');
+//         })
+//     });
+// }
+
+// // getCountryAndNeighbour('venezuela');
+// getCountryAndNeighbour('usa');
+
+// // Callback hell example
+// setTimeout(() => {
+//     console.log('1 seconds passed');
+//     setTimeout(() => {
+//         console.log('2 seconds passed');
+//         setTimeout(() => {
+//             console.log('3 seconds passed');
+//         setTimeout(() => {
+//             console.log('4 seconds passed');
+//             }, 1000)
+//         }, 1000)
+//     }, 1000)
+// }, 1000)
+
+
+
+// Promises and Fetch API
+// const request = new XMLHttpRequest();
+// request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+// request.send();
+
+// const request = fetch(`https://restcountries.com/v3.1/name/venezuela`);
+// console.log(request);
+
+// Extended version
+// const getCountryData = function (country) {
+//     fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(function (response) {
+//         console.log(response);
+//         return response.json();
+//     })
+//     .then (function (data) {
+//         console.log(data);
+//         renderCountry(data[0]);
+//     });
+// }
+
+// Simplified version
+const getCountryData = function (country) {
+    fetch(`https://restcountries.com/v3.1/name/${country}`)
+        .then(response => response.json())
+        .then (data => renderCountry(data[0]));
 }
 
-// getCountryAndNeighbour('venezuela');
-getCountryAndNeighbour('usa');
-
-// Callback hell example
-setTimeout(() => {
-    console.log('1 seconds passed');
-    setTimeout(() => {
-        console.log('2 seconds passed');
-        setTimeout(() => {
-            console.log('3 seconds passed');
-        setTimeout(() => {
-            console.log('4 seconds passed');
-            }, 1000)
-        }, 1000)
-    }, 1000)
-}, 1000)
+getCountryData('venezuela');
