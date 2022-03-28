287e6056b9256f012a7d1c1fba296e987b94274c
ewa1do <this.eduardovera@gmail.com>
2022-03-28 08:31:52 -0400

    coding challenge #1

--- 16 - Asynchronous Javascript/script.js
+++ 16 - Asynchronous Javascript/script.js
@@ -5,7 +5,7 @@ const countriesContainer = document.querySelector('.countries');
 
 const renderError = function (msg) {
     countriesContainer.insertAdjacentText('beforeend', msg);
-    // countriesContainer.style.opacity = 1;
+    countriesContainer.style.opacity = 1;
 }
 
 const renderCountry = function (data, className = '') {
@@ -24,7 +24,7 @@ const renderCountry = function (data, className = '') {
     `;
 
     countriesContainer.insertAdjacentHTML('beforeend', html);
-    // countriesContainer.style.opacity = '1';
+    countriesContainer.style.opacity = '1';
 }
 
 // const getCountryData = function (country) {
@@ -220,10 +220,87 @@ const getCountryData = function (country) {
     })
 }
 
-
 btn.addEventListener('click', function () {
     getCountryData('venezuela');
 });
 
-// getCountryData('skjdskjdks');
+///////////////////////////////////////////////
+// Coding Challenge #1
 
+/* 
+In this challenge you will build a function 'whereAmI' which renders a
+country ONLY based on GPS coordinates. For that, you will use a second 
+API to geocode coordinates.
+
+Here are your tasks:
+
+PART 1
+1. Create a function 'whereAmI' which takes an inputs a latitude value (lat)
+and a longitude value (lng) (these are coordinates, examples are below).
+2. Do 'reverse geocoding' of the provided. Reverse geocoding means to 
+convert coodinates to a meaningful location, like a city and country name.
+Use this API to do reverse geocoding: https://geocode.xyz/api.
+The AJAX call will be done to a URL with this format: 
+https://geocode.xyz/52.508,12.381?geoit=json. Use the fetch API and promises
+to get the data.
+3. Once you have the data, take a look at it in the console to see all the 
+attributes that you received about the provided location. Then, using this 
+data, log a message like this in the console: 'You are in Berlin, Germany'
+4. Chain a .catch method to the end of the promise chain and log errors 
+to the console.
+5. This API allows you to make only 3 request per second. If you reload fast,
+you will get this error with code 403. This is an error with the request.
+Remember, fetch() does NOT reject the promise in this case. So create an error 
+to reject promise yourself, with a meaningful error message.
+
+PART 2
+6. Now it's time to use the received data to render a country. So take the 
+relevant attribute from the geocoding API result, and plug it into the countries
+API that we have been using.
+7. Render the country and catch any errors, just like we have done in the last lecture
+(you can even copy this code, no need to type the same code)
+
+TEST COORDINATES 1: 52.508, 12.381 (latitude, longitude)
+TEST COORDINATES 2: 19.037, 72.873
+TEST COORDINATES 3: -33.933, 18.474
+*/
+
+const whereAmI = function (lat, lng) {
+    fetch (`https://geocode.xyz/${lat},${lng}?geoit=json&auth=221622119368574280400x62654`)
+    .then (response => {
+        if (!response.ok) throw new Error(`Problem with Geocoding ${response.status}`);
+
+        return response.json();
+    })
+    .then (data => {
+        if (Object.keys(data).some(key => key === 'error'))
+            throw new Error(`${data.error.description}`);
+
+        return fetch (`https://restcountries.com/v3.1/name/${data.country}`)
+    })
+    .then (res => {
+        if (!res.ok) throw new Error(`Country not found ${res.status}`)
+        return res.json()
+    })
+    .then (data => {
+        renderCountry(data[0]);
+        const [neighbour] = data[0].borders;
+        return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then (res => {
+        if (!res.ok) throw new Error(`Country has no neighbour ${res.status}`);
+        return res.json();
+    })
+    .then (data => renderCountry(data[0], 'neighbour'))
+    .catch (err => {
+        console.error(`🔥🔥🔥 Something went wrong: ${err}`);
+        renderError(`🔥🔥🔥 Something went wrong: ${err}`);
+    })
+    // .finally (() => countriesContainer.style.opacity = 1);
+}
+
+whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.474);
+// whereAmI(10.647,-71.617);
+// whereAmI(13.037, 72.873);
