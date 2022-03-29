42b5e1572bced1e0e87f55a8b1e048b4d5684462
ewa1do <this.eduardovera@gmail.com>
2022-03-29 14:21:25 -0400

    coding challenge #2

--- 16 - Asynchronous Javascript/script.js
+++ 16 - Asynchronous Javascript/script.js
@@ -410,3 +410,76 @@ const whereAmI = function () {
 }
 
 btn.addEventListener('click', whereAmI);
+
+//////////////////////////////////////////////////////////
+// Coding Challenge #2
+
+/*
+PART 1:
+1. Create a function 'createImage' which receives imgPath as an input. 
+This function returns a promise which creates a new image (use 
+document.createElement('img')) and sets the .src attribute to the 
+provided img path. When the image is donde loading, append it to the DOM 
+element with the 'images' class, and resolve the promise. The fulfilled value
+should be the image element itself. In case there is an error loading the image
+('error' event), reject the promise.
+
+PART 2:
+2. Consume the promise using .then and also add an error handler.
+3. After the image has loaded, pause execution for 2 seconds using the wait 
+function we created earlier.
+4. After the 2 seconds have passed, hide the current image (set display to 'none'),
+and load a second image (HINT: use the image element returned by the createImage 
+promise to hide the current image. You will need a global variable for that)
+5. After the 2 seconds have passed, hide the current image.
+
+TEST DATA: images in the img folder. Test the error handler by passin a wrong 
+image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise
+images load too fast.
+*/
+
+const imgDiv = document.querySelector('.images');
+
+const wait = (seconds) => {
+    return new Promise (resolve => {
+        setTimeout(resolve, seconds * 1000);
+    });
+}
+
+const createImage = function (imgPath) {
+    return new Promise (function (resolve, reject) {
+        const img = document.createElement('img');
+        img.src = imgPath;
+
+        img.addEventListener('load', function () {
+            imgDiv.appendChild(img);
+            resolve(img);
+        })
+        // resolve(imgDiv.appendChild(img));
+
+        img.addEventListener('load', function () {
+            reject(new Error('Image not found'));
+        })
+    })
+} 
+
+let currentImg;
+
+createImage('img/img-1.jpg')
+.then(res => {
+    currentImg = res;
+    return wait(2);
+})
+.then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+})
+.then(res => {
+    currentImg = res;
+    return wait(2);
+})
+.then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-3.jpg');
+})
+.catch(err => console.error(err));
