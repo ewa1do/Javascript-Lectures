3fd8a45f5284df1fed8c761380cd4fcca28bc71c
ewa1do <this.eduardovera@gmail.com>
2022-04-02 09:14:34 -0400

    CommonJS Modules

--- 17 - Modern Javascript Development/script.js
+++ 17 - Modern Javascript Development/script.js
@@ -52,31 +52,42 @@ console.log(cart);
 // console.log(lastPost2);
 
 // The module pattern
-const ShoppingCart2 = (function () {
-    const cart = [];
-    const shippingCost = 10;
-    const totalPrice = 237;
-    const totalQuantity = 23;
+// const ShoppingCart2 = (function () {
+//     const cart = [];
+//     const shippingCost = 10;
+//     const totalPrice = 237;
+//     const totalQuantity = 23;
 
-    const addToCart = function (product, quantity) {
-        cart.push({ product, quantity });
-        console.log(`${quantity} ${product} added to cart, (shipping cost is ${shippingCost})`);
-    }
+//     const addToCart = function (product, quantity) {
+//         cart.push({ product, quantity });
+//         console.log(`${quantity} ${product} added to cart, (shipping cost is ${shippingCost})`);
+//     }
 
-    const orderStock = function (product, quantity) {
-        console.log(`${quantity} ${product} orderer from supplier`);
-    }
+//     const orderStock = function (product, quantity) {
+//         console.log(`${quantity} ${product} orderer from supplier`);
+//     }
 
-    return {
-        addToCart,
-        cart,
-        totalPrice,
-        totalQuantity
-    }
-})();
+//     return {
+//         addToCart,
+//         cart,
+//         totalPrice,
+//         totalQuantity
+//     }
+// })();
 
-ShoppingCart2.addToCart('apple', 4);
-ShoppingCart2.addToCart('pizza', 2);
-console.log(ShoppingCart2);
-console.log(ShoppingCart2.shippingCost);
+// ShoppingCart2.addToCart('apple', 4);
+// ShoppingCart2.addToCart('pizza', 2);
+// console.log(ShoppingCart2);
+// console.log(ShoppingCart2.shippingCost);
 
+
+// CommonJs Modules --> Mostly used in NodeJS
+
+// Exporting
+export.addToCart =  function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart, (shipping cost is ${shippingCost})`);
+}    
+
+// Importing
+const { addToCart } = require('./shoppingCart.js');
