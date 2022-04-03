7e48ad435afd870d543aae10cd07c59257d05cfc
ewa1do <this.eduardovera@gmail.com>
2022-04-03 11:39:22 -0400

    Writing clean and modern javascript

---
+++ 17 - Modern Javascript Development/clean.js
@@ -0,0 +1,48 @@
+const budget = [
+  { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
+  { value: -45, description: 'Groceries 🥑', user: 'jonas' },
+  { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
+  { value: 300, description: 'Freelancing 👩‍💻', user: 'jonas' },
+  { value: -1100, description: 'New iPhone 📱', user: 'jonas' },
+  { value: -20, description: 'Candy 🍭', user: 'matilda' },
+  { value: -125, description: 'Toys 🚂', user: 'matilda' },
+  { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
+];
+
+const spendingLimits = {
+  jonas: 1500,
+  matilda: 100,
+};
+
+const getLimit = user => spendingLimits?.[user] ?? 0;
+
+const addExpense = function (value, description, user = 'Jonas') {
+  user = user.toLowerCase();
+
+  if (value <= getLimit(user)) {
+    budget.push({ value: -value, description, user });
+  }
+};
+addExpense(10, 'Pizza 🍕');
+addExpense(100, 'Going to movies 🍿', 'Matilda');
+addExpense(200, 'Stuff', 'Jay');
+
+const checkExpenses = function () {
+  for (const entry of budget)     
+    if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
+};
+checkExpenses();
+
+
+
+const logBigExpenses = function (limit) {
+  let output = '';
+  for (let entry of budget) 
+      output += entry.value <= -limit ? `${entry.description.slice(-2)} / ` : ''; // Emojis are 2 chars
+
+  output = output.slice(0, -2); // Remove last '/ '
+  console.log(output);
+};
+
+console.log(budget);
+logBigExpenses(500);
