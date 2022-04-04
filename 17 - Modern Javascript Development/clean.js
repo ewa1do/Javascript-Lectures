ba677165cdb6cba6c32a0a27d0e5cf6ad5056ced
ewa1do <this.eduardovera@gmail.com>
2022-04-04 11:12:48 -0400

    fixing bad code part 2

--- 17 - Modern Javascript Development/clean.js
+++ 17 - Modern Javascript Development/clean.js
@@ -1,4 +1,7 @@
-const budget = [
+'use strict';
+
+//             making the object inmmutable
+const budget = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -7,42 +10,72 @@ const budget = [
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
 
-const spendingLimits = {
+const spendingLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
+});
+// spendingLimits.jay = 200;
+console.log(spendingLimits);
+
+// const getLimit = user => spendingLimits?.[user] ?? 0;
+const getLimit = (limits, user) => limits?.[user] ?? 0;
+
+// Pure function :D - No side effects
+const addExpense = function (state, limits, value, description, user = 'Jonas') {
+  const cleanUser = user.toLowerCase();
+
+  // if (value <= getLimit(cleanUser)) {
+  //   return [...state, { value: -value, description, user: cleanUser }];
+
+  //   // budget.push({ value: -value, description, user: cleanUser });
+  // }
+
+  return value <= getLimit(limits, cleanUser) 
+  ? [...state, { value: -value, description, user: cleanUser }] 
+  : state;
+  
 };
+const newBudget1 = addExpense(budget, spendingLimits, 10, 'Pizza 🍕'); 
+                              // notice that im returning in the state the return of the first function, because in the original
+                              // they won't appear
+const newBudget2 = addExpense(newBudget1, spendingLimits, 100, 'Going to movies 🍿', 'Matilda');
+const newBudget3 = addExpense(newBudget2, spendingLimits, 200, 'Stuff', 'Jay');
 
-const getLimit = user => spendingLimits?.[user] ?? 0;
+// console.log(newBudget3);
 
-const addExpense = function (value, description, user = 'Jonas') {
-  user = user.toLowerCase();
+// const checkExpenses2 = function (state, limits) {
+//   return state.map(entry => {
+//     return entry.value < -getLimit(limits, entry.user) 
+//     ? { ...entry, flag: 'limit' } : entry;
+//   });
 
-  if (value <= getLimit(user)) {
-    budget.push({ value: -value, description, user });
-  }
-};
-addExpense(10, 'Pizza 🍕');
-addExpense(100, 'Going to movies 🍿', 'Matilda');
-addExpense(200, 'Stuff', 'Jay');
+//   // for (const entry of budget)     
+//   // if (entry.value < -getLimit(limits, entry.user)) entry.flag = 'limit';
+// };
 
-const checkExpenses = function () {
-  for (const entry of budget)     
-    if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
-};
-checkExpenses();
+const checkExpenses = (state, limits)  => 
+  state.map(entry => 
+    entry.value < -getLimit(limits, entry.user) 
+    ? { ...entry, flag: 'limit' } : entry
+  );
 
+const finalBudget = checkExpenses(newBudget3, spendingLimits);
+console.log(finalBudget);
 
 
-const logBigExpenses = function (limit) {
-  let output = '';
-  for (let entry of budget) 
-      output += entry.value <= -limit ? `${entry.description.slice(-2)} / ` : ''; // Emojis are 2 chars
+// const logBigExpenses = function (state, limit) {
+//   return state.filter(entry => entry.value <= -limit)
+//     .map(entry => entry.description.slice(-2)).join(' / ');
+//     // .reduce((str, cur) => `${str} / ${cur.description.slice(-2)}` ,'');
+//   // console.log(bigExpenses);
+// };
 
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
-};
+const logBigExpenses = (state, limit) => 
+  state.filter(entry => entry.value <= -limit)
+  .map(entry => entry.description.slice(-2))
+  .join(' / ');
 
-console.log(budget);
-logBigExpenses(500);
+  // impure
+console.log(logBigExpenses(finalBudget, 500));
