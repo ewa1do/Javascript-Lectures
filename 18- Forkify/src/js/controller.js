b50158446342ebea8ae49c71d5951ff623b46dd3
ewa1do <this.eduardovera@gmail.com>
2022-04-04 12:47:52 -0400

    Loading a Recipe from API

--- 18- Forkify/src/js/controller.js
+++ 18- Forkify/src/js/controller.js
@@ -1,3 +1,5 @@
+// const { re } = require("prettier");
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,3 +13,37 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604691c37cdc054bd0bc');
+    // const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+
+    const {data} = await res.json();
+    
+    if (!res.ok) throw new Error(`Status (${res.status}): ${data.message}`);
+
+    // console.log(res, data);
+
+    let {recipe} = data;
+
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients
+    }
+
+    console.log(recipe);
+
+  } catch (err) {
+    console.log(err)
+    alert(err)
+  }
+}
+
+showRecipe();
