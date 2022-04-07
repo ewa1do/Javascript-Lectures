784bb72fc9fa9573e7e885fd89c1d3ce4ec81b18
ewa1do <this.eduardovera@gmail.com>
2022-04-06 20:16:03 -0400

    implementing search results part 2

--- 18- Forkify/src/js/views/recipeView.js
+++ 18- Forkify/src/js/views/recipeView.js
@@ -1,78 +1,25 @@
+import View from './View.js';
+
 // import icons from 'url:../img/icons.svg'; // Parcel 2
 import icons from '../../img/icons.svg'; // Parcel 1
 import fracty from 'fracty';
 
-// console.log(fracty(0.5));
 
-class RecipeView {
-    #parentElement = document.querySelector('.recipe');
-    #data;
-    #errorMessage = `We couldn't find that recipe. Please try another one!`;
-    #message = '';
-    
-    render (data) {
-        this.#data = data;
-        const markup = this.#generateMarkup();
-        this.#clear();
-        this.#parentElement.insertAdjacentHTML('afterbegin', markup);
-    }
-
-    #clear () {
-        this.#parentElement.innerHTML = '';
-    }
-
-    renderSpinner () {
-        const markup = `
-        <div class="spinner">
-            <svg>
-            <use href="${icons}#icon-loader"></use>
-            </svg>
-        </div>
-        `;
-        this.#clear;
-        this.#parentElement.insertAdjacentHTML('afterbegin', markup);
-    }
-
-    renderError (message = this.#errorMessage) {
-        const markup = `
-            <div class="error">
-                <div>
-                    <svg>
-                    <use href="${icons}#icon-alert-triangle"></use>
-                    </svg>
-                </div>
-                <p>${message}</p>
-            </div>
-        `;
-        this.#clear();
-        this.#parentElement.insertAdjacentHTML('afterbegin', markup);
-    }
-
-    renderMessage (message = this.#message) {
-        const markup = `
-            <div class="message">
-                <div>
-                    <svg>
-                    <use href="${icons}#icon-smile"></use>
-                    </svg>
-                </div>
-                <p>${message}</p>
-            </div>
-        `;
-        this.#clear();
-        this.#parentElement.insertAdjacentHTML('afterbegin', markup);
-    }
+class RecipeView extends View {
+    _parentElement = document.querySelector('.recipe');
+    _errorMessage = `We couldn't find that recipe. Please try another one!`;
+    _message = '';
 
     addHandlerRender (handler) {
         ['load', 'hashchange'].forEach(ev => window.addEventListener(ev, handler));
     } 
 
-    #generateMarkup () {
+    _generateMarkup () {
     return `
         <figure class="recipe__fig">
-            <img src="${this.#data.image}" alt="${this.#data.title}" class="recipe__img" />
+            <img src="${this._data.image}" alt="${this._data.title}" class="recipe__img" />
             <h1 class="recipe__title">
-            <span>${this.#data.title}</span>
+            <span>${this._data.title}</span>
             </h1>
         </figure>
 
@@ -81,14 +28,14 @@ class RecipeView {
         <svg class="recipe__info-icon">
             <use href="${icons}#icon-clock"></use>
         </svg>
-        <span class="recipe__info-data recipe__info-data--minutes">${this.#data.cookingTime}</span>
+        <span class="recipe__info-data recipe__info-data--minutes">${this._data.cookingTime}</span>
         <span class="recipe__info-text">minutes</span>
         </div>
         <div class="recipe__info">
         <svg class="recipe__info-icon">
             <use href="${icons}#icon-users"></use>
         </svg>
-        <span class="recipe__info-data recipe__info-data--people">${this.#data.servings}</span>
+        <span class="recipe__info-data recipe__info-data--people">${this._data.servings}</span>
         <span class="recipe__info-text">servings</span>
 
         <div class="recipe__info-buttons">
@@ -106,9 +53,6 @@ class RecipeView {
         </div>
 
         <div class="recipe__user-generated">
-            <svg>
-                <use href="${icons}#icon-user"></use>
-            </svg>
         </div>
         <button class="btn--round">
         <svg class="">
@@ -120,7 +64,7 @@ class RecipeView {
         <div class="recipe__ingredients">
             <h2 class="heading--2">Recipe ingredients</h2>
             <ul class="recipe__ingredient-list">
-                ${this.#data.ingredients.map(this.#generateMarkupIngredient).join('')}
+                ${this._data.ingredients.map(this._generateMarkupIngredient).join('')}
             </ul>
         </div>
 
@@ -128,10 +72,10 @@ class RecipeView {
             <h2 class="heading--2">How to cook it</h2>
             <p class="recipe__directions-text">
                 This recipe was carefully designed and tested by
-                <span class="recipe__publisher">${this.#data.publisher}</span>. Please check out
+                <span class="recipe__publisher">${this._data.publisher}</span>. Please check out
                 directions at their website.
             </p>
-            <a class="btn--small recipe__btn" href="${this.#data.sourceUrl}" target="_blank">
+            <a class="btn--small recipe__btn" href="${this._data.sourceUrl}" target="_blank">
                 <span>Directions</span>
                 <svg class="search__icon">
                 <use href="${icons}#icon-arrow-right"></use>
@@ -141,7 +85,7 @@ class RecipeView {
     `;
     }
 
-    #generateMarkupIngredient (ing) {
+    _generateMarkupIngredient (ing) {
         return `
             <li class="recipe__ingredient">
             <svg class="recipe__icon">
--- 18- Forkify/src/js/views/searchView.js
+++ 18- Forkify/src/js/views/searchView.js
@@ -1,18 +1,18 @@
 class SearchView {
-    #parentElement = document.querySelector('.search');
+    _parentElement = document.querySelector('.search');
 
     getQuery () {
-        const query = this.#parentElement.querySelector('.search__field').value;
-        this.#clearInput();
+        const query = this._parentElement.querySelector('.search__field').value;
+        this._clearInput();
         return query;
     }
 
-    #clearInput () {
-        this.#parentElement.querySelector('.search__field').value = '';
+    _clearInput () {
+        this._parentElement.querySelector('.search__field').value = '';
     }
 
     addHandlerSearch (handler) {
-        this.#parentElement.addEventListener('submit', function (e) {
+        this._parentElement.addEventListener('submit', function (e) {
             e.preventDefault();
             handler();
         })
